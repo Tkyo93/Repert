0f7fe4f6d7fba40b6992f4fa993966af5d05478e
Gloire <gloire@gmail.com>
2026-05-21 18:04:54 +0200

    mis a niveau avant responsives !

--- projet/Assets/js/index.js
+++ projet/Assets/js/index.js
@@ -1,15 +1,4 @@
 
-            // chargement
-            // Attend que tout soit chargé (images, CSS, etc.)
-            window.addEventListener("load", function () {
-                // Cache le loader
-                document.getElementById("loader").style.display = "none";
-
-                // Affiche le contenu principal
-                document.getElementById("content").classList.remove("hidden");
-                document.getElementById("content").classList.add("show");
-            });
-
             // blocs
 
             function ouvrir() {
